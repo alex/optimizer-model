3205c6e9aeb289bd31c26537f9bb25a55952d56e
Alex Gaynor <alex.gaynor@gmail.com>
2013-06-01 11:44:15 -0700

    more coverage on intbounds

--- tests/test_intbounds.py
+++ tests/test_intbounds.py
@@ -54,3 +54,19 @@ class TestIntBounds(object):
         assert i2.has_lower()
         assert i2.get_upper() == 9
         assert i2.get_lower() == 1
+
+    def test_make_le_already_bounded(self):
+        i0 = IntUnbounded()
+        i1 = i0.make_le(ConstantIntBounds(0)).make_le(ConstantIntBounds(2))
+
+        assert i1.has_upper()
+        assert not i1.has_lower()
+        assert i1.get_upper() == 0
+
+    def test_make_ge_already_bounded(self):
+        i0 = IntUnbounded()
+        i1 = i0.make_ge(ConstantIntBounds(10)).make_ge(ConstantIntBounds(0))
+
+        assert i1.has_lower()
+        assert not i1.has_upper()
+        assert i1.get_lower() == 10
